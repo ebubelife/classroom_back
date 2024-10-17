236fb4b03afe46d48c5b210014d0cfdddf977701
ebubelife <ebubeemeka19@gmail.com>
2024-10-17 10:41:06 +0100

    ...

--- app/Models/AdminMain.php
+++ app/Models/AdminMain.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class AdminMain extends Model
+class AdminMain extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'admin_users';
-    
+
+    protected $fillable = [
+        'name',
+        'username',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
 }
